772b3a44e0f918a06729ee2edcb10e157a051e90
Srujana <srujanamamillapalli38@gmail.com>
2026-01-03 10:37:55 +0530

    introduced bug in view_tasks

--- todo.py
+++ todo.py
@@ -1,11 +1,2 @@
-
 def view_tasks():
-    if not tasks:
-        print("No tasks available")
-    for i, task in enumerate(tasks, 1):
-        print(i, task)
-
-def add_task(task):
-    tasks.append(task)
-    print("Task added")
-
+    print(tasks[10])   # BUG: index out of range
